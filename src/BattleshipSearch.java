a0f82a73be3dc2e4f254e8b7c0b309253714146d
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-01-25 15:53:08 -0600

    Created grid

--- src/BattleshipSearch.java
+++ src/BattleshipSearch.java
@@ -1,8 +1,47 @@
 public class BattleshipSearch {
 	SearchStrategy searchStrategy;
-	
+	boolean[][] grid = new boolean[25][25];
+		
 	public BattleshipSearch() 
 	{
 		
+	}
+	
+	public void initGrid() {
+		for (int i = 0; i < grid.length; i++) 
+		{
+			for (int j = 0; j < grid.length; j++)
+			{
+				setGridLocation(i,j,false);
+			}
+		}
+	}
+	
+	public boolean[][] getGrid()
+	{
+		return grid;
+	}
+	
+	public void setGridLocation(int i, int j, boolean b) 
+	{
+		grid[i][j] = b;
+	}
+	
+	public void displayGrid() {
+		for (int i = 0; i < grid.length; i++) 
+		{
+			for (int j = 0; j < grid.length; j++)
+			{
+				int b = grid[i][j] ? 1 : 0;
+				System.out.print("|" + b);
+			}
+		
+			System.out.print("|"+'\n');
+			for (int k = 0; k <= grid.length * 2; k++)
+			{
+				System.out.print("-");
+			}
+			System.out.print('\n');
+		}
 	}
 }
--- src/BattleshipTester.java
+++ src/BattleshipTester.java
@@ -1,3 +1,7 @@
-public class BattleshipTester {
+public class BattleshipTester 
+{
+	public static void main(String[] args) 
+	{
 
+	}
 }
